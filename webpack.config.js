3ffe44ab8d7ed74e3508504be9dddb66abc4f531
Anthony Lord <antholord@hotmail.com>
2017-04-13 15:36:51 -0400

    trying to make it work

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   module: {
     loaders: [{
       exclude: /node_modules/,
-      loader: 'babel-loader',
+      loader: 'babel',
       query: {
         presets: ['react', 'es2015', 'stage-1']
       }

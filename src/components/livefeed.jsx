19a88792200181a1f900ebbfe69c2da34722c451
Anthony Lord <antholord@hotmail.com>
2017-04-14 12:12:29 -0400

    testing to put data in a table

--- src/components/livefeed.jsx
+++ src/components/livefeed.jsx
@@ -21,23 +21,25 @@ class LiveFeed extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {msg : "test"};
+    this.state = {rows : [], msg : "test", league : "Legacy", type : "Ancient%20Reliquary%20Key"};
   }
 
     handleData(data) {
-        //let result = JSON.parse(data);
-        console.log(data);
-        let result = data;
+        let result = JSON.parse(data);
+        console.log(result);
+        //console.log(data);
+        //let result = data;
+        this.setState({rows : this.state.rows.push(result)});
         this.setState({msg: this.state.msg + "\n" + data});
     }
 
     render() {
         return (
-            <div>
-              Live search: <strong>{this.state.msg}</strong>
-              <Websocket url='wss://poe-livesearch-api.herokuapp.com/ws/livesearch?league=Legacy&type=Ancient%20Reliquary%20Key'
+            //<div>
+              //Live search: <strong>{this.state.msg}</strong>
+              <Websocket url={'wss://poe-livesearch-api.herokuapp.com/ws/livesearch?league=' + this.state.league + '&type=' + this.state.type}
                          onMessage={this.handleData.bind(this)}/>
-            </div>
+            //</div>
         );
     }
 

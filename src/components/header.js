82aabb6dc4a2643db097f2bb24e86325c3811267
Anthony Lord <antholord@hotmail.com>
2017-05-02 15:39:53 -0400

    Updated interface

--- src/components/header.js
+++ src/components/header.js
@@ -15,12 +15,13 @@ const Header = ({league, onLeagueChange}) => {
         <nav className="navbar customNav colored">
             <div className="container ">
                 <a className="navbar-brand" href="/">Path of Exile Live Search</a>
-                <DropdownButton onSelect={onLeagueChange} title={league} id="leagueDropdown" className="dropdown">
+
+                <DropdownButton onSelect={onLeagueChange} title={league} id="leagueDropdown" className="dropdown navbar-left">
                     <MenuItem eventKey="Legacy">Legacy</MenuItem>
                     <MenuItem eventKey="Hardcore Legacy">Hardcore Legacy</MenuItem>
                 </DropdownButton>
+                <h4 className="navbar-text">Alpha version</h4>
                 <ul className="nav navbar-nav navbar-right">
-
                     <li className="navLink"><a href="/about">About</a></li>
                 </ul>
             </div>
